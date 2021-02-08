cb4aa8c342e0f9b2e5133ce9e03370cb339f9382
SenpaizX666 <77642088+SenpaizX666@users.noreply.github.com>
2021-02-08 20:25:46 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,164 @@
+const help = (prefix) => {
+	return`⚡▬▬▬▬▬ [ BOT ] ▬▬▬▬▬⚡
+
+                    LostzXテ BOT
+
+⚡▬▬▬▬▬ [ BOT ] ▬▬▬▬▬⚡
+
+
+➸   𝚘 𝚠 𝚗 𝚎 𝚛 : LostzXテ
+➸  𝚙 𝚛 𝚎 𝚏 𝚒 𝚡 :「 ${prefix} 」
+
+
+*Use !teste para ver se o bot está on,
+se ele estiver vai te responder*
+
+
+
+          「 *COMANDOS DE INTERAÇÃO* 」
+
+
+
+${prefix}minigames (minijogos simples)
+
+${prefix}apks (apps e jogos premium)
+
+${prefix}group (grupo do meu criador)
+
+${prefix}notas (notas de atualização do bot)
+
+${prefix}tabela (tabela de símbolos e letras)
+
+
+
+           「 *COMANDOS DE STICKERS* 」
+
+
+
+${prefix}sticker (converte imagem em fig)
+
+${prefix}toimg (converte fig em imagem)
+
+${prefix}stickergif (converte gif em fig)
+
+
+
+            「 *COMANDOS DE ANIME* 」
+
+
+
+${prefix}randomanime (anime aleatório)
+
+${prefix}loli (fotos aleatórias de loli)
+
+${prefix}waifu (fotos aleatórias de waifus)
+
+${prefix}pokemon (fotos aleatórias de pokemons)
+
+${prefix}wait (pesquisa o anime pela foto)
+
+
+
+           「 *COMANDOS DE HENTAI* 」
+
+
+
+${prefix}nsfw 1 (para ativar o modo hentai no grupo)
+
+${prefix}nsfw 0 (para desativar o modo hentai no grupo)
+
+${prefix}randomhentaio (hentai aleatório)
+
+${prefix}nsfwbobs (nsfwbobs aleatório)
+
+${prefix}nsfwloli (nsfwloli aleatória)
+
+${prefix}nsfwass (nsfwass aleatório)
+
+${prefix}nsfwtrap (nsfwtrap aleatório)
+
+${prefix}nsfwbelly (nsfwbelly aleatória)
+
+${prefix}nsfwfeets (nsfwfeets aleatório)
+
+${prefix}nsfwhegao (nsfwhegao aleatório)
+
+${prefix}nsfwsidebobs (nsfwsidebobs aleatório)
+
+${prefix}nsfwthighs (nsfwthighs aleatório)
+
+${prefix}nsfwarmpits (nsfwarmpits aleatório)
+
+${prefix}porn (mais de 100 sites pornô)
+
+${prefix}pack (pack pornô pelo mega)
+
+
+
+             「 *COMANDOS DE ÁUDIO* 」
+
+
+
+${prefix}tts it (converte texto em áudio)
+
+${prefix}play (pesquisa e baixa uma música)
+
+
+
+              「 *COMANDOS DE TEXTO* 」
+
+
+
+${prefix}logomaker (menu de converter texto em logos)
+
+${prefix}ocr (pega o texto da imagem)
+
+
+
+             「 *COMANDOS DE ADMIN* 」
+
+
+
+${prefix}add
+${prefix}kick
+${prefix}welcome (1/0)
+${prefix}tagall (1/2/3/4/5)
+${prefix}promote
+${prefix}demote
+
+
+
+                 「 *OUTROS COMANDOS* 」
+
+
+
+${prefix}tiktokstalk
+${prefix}ytmp4
+${prefix}images
+${prefix}meme
+${prefix}info
+${prefix}blocklist
+${prefix}ping
+${prefix}owner
+
+
+⚡▬▬▬▬▬▬ [ BOT ] ▬▬▬▬▬▬⚡
+
+Quer por o bot no seu grupo e poder fazer transmissões,pedir pra por um comando próprio de você que todos possam usar,e mais? Use "adc" e veja
+
+Quer aprender a criar seu próprio bot igual ao meu e ganhar dinheiro mesmo não sabendo nada de programação? Use "quero"
+
+⚡▬▬▬▬▬▬ [ BOT ] ▬▬▬▬▬▬⚡
+
+
+
+
+====================================
+
+                      「 by @lostzx.mp4 」
+
+====================================`
+
+}
+
+exports.help = help
